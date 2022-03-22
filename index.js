1a293d3fb52a8214cb7a4ab5817a9671f45444f2
Eshwar Seerdat <eshwarseerdat@gmail.com>
2022-03-22 06:49:59 -0400

    Form and route for new product created

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -23,6 +24,17 @@ app.get("/", (req, res) => {
 app.get("/products", async (req, res) => {
   const allProducts = await Product.find({});
   res.render("product/index", { allProducts });
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("product/new");
+});
+
+app.post("/products", async (req, res) => {
+  const { name, price, category } = req.body;
+  const newProduct = new Product({ name, price, category });
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get("/products/:id", async (req, res) => {
